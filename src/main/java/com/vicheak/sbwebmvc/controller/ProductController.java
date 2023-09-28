278b09d3989b65e250e1b940b2cf7b14c4687a6d
Vicheak <suonvicheak991@gmail.com>
2023-09-28 19:15:06 +0700

    Add ProductService and Implementation

--- src/main/java/com/vicheak/sbwebmvc/controller/ProductController.java
+++ src/main/java/com/vicheak/sbwebmvc/controller/ProductController.java
@@ -3,56 +3,34 @@ package com.vicheak.sbwebmvc.controller;
 import com.vicheak.sbwebmvc.model.Product;
 import com.vicheak.sbwebmvc.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//front controller notices this controller
-@RestController //all return type is serialized accordingly to JSON data format
-@RequestMapping("/products") //group as class level
+@RestController
+@RequestMapping("/products")
 @RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
 
-    //this is a mapping url with get method
-    //@RequestMapping(method = RequestMethod.GET, value = "/products")
-    //method attribute : used with type safe
-
-    //@GetMapping("/products")
-    //@GetMapping
     @GetMapping(produces = "application/json")
     public List<Product> getProducts() {
-        //return "Retrieve Products";
         return productService.loadProducts();
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public Product createNewProduct(@RequestBody Product product) {
-        //return "Create new product";
-        //System.out.println(product);
-
-        /*Product newProduct = Product.builder()
-                .id(5)
-                .name("Gaming Chair 2023")
-                .price(180.00)
-                .inStock(true)
-                .build();*/
-        return productService.addProduct(product);
+    public void createNewProduct(@RequestBody Product product) {
+        productService.createNewProduct(product);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    public Product updateProduct(@PathVariable Integer id,
-                                 @RequestBody Product newProduct) {
-        //return "Update product";
-
-        /*Product newProduct = Product.builder()
-                .id(4)
-                .name("Magic Mouse 2")
-                .price(210.99)
-                .inStock(true)
-                .build();*/
-        return productService.updateProduct(id, newProduct);
+    public void updateProduct(@PathVariable Integer id,
+                              @RequestBody Product newProduct) {
+        productService.updateProductById(id, newProduct);
     }
 
     @PatchMapping
@@ -60,17 +38,15 @@ public class ProductController {
         return "Update product partially";
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deleteProduct(@PathVariable Integer id) {
         productService.deleteProductById(id);
     }
 
-    //@GetMapping("/{id}") //create placeholder url for path variable (dynamic)
     @GetMapping("/{id}/{slug}")
     public Product getProductById(@PathVariable("id") Integer proId,
                                   @PathVariable String slug) {
-        System.out.println("Product ID : " + proId);
-        System.out.println("Product Slug : " + slug);
         return productService.loadProductById(proId);
     }
 
@@ -79,7 +55,7 @@ public class ProductController {
                                        @RequestParam(required = false, defaultValue = "true") Boolean status) {
         System.out.println("Name : " + name);
         System.out.println("Status : " + status);
-        return productService.searchProduct(name, status);
+        return null;
     }
 
 }
